7f3e1079d92e401ec4fc5e3cf4ddf083ab254d41
Orayanee Janjaeng <orayaneejj@gmail.com>
2024-10-31 13:43:50 +0700

    refactor: add useBlogPosts custom hook for fetching blog posts

---
+++ client/src/hook/useBlogPosts.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useBlogPosts() {
+  const [posts, setPosts] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getPosts = async () => {
+    try {
+      setIsError(false);
+      setIsLoading(true);
+      const results = await axios("http://localhost:4000/posts");
+      setPosts(results.data.data);
+    } catch (error) {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return { posts, isError, isLoading };
+}
+
+export default useBlogPosts;
